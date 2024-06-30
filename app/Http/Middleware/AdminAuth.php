1b26b2e7f218d74d9235f90561588c240f8c48cb
Success Drame <d.ije95@gmail.com>
2024-06-30 09:32:32 +0100

    Admin AuthMiddleware

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,13 +16,13 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $allowedRoles = [1,2]; // Replace with the desired role(s)
+        $allowedRoles = [1]; // Replace with the desired role(s)
 //        $userRoles = json_decode(Auth::user()->user_role, true); // $roles will be an array
         $userRoles =Auth::user()->user_role; // $roles will be an array
 
         if (is_array($userRoles) && array_intersect($allowedRoles, $userRoles) && Auth::user()->account_type=='internal') {
             return $next($request);
         }
-        return jsonResponse('Invalid Admin Credentials', [], 403);
+        return jsonResponse('Invalid Admin Credentials', [], Response::HTTP_FORBIDDEN);
     }
 }
